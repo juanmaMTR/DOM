530557862821b74b70abb74752eb0a78bfb999e4
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-10-08 11:04:57 +0200

    Commit 1

---
+++ js/dom.js
@@ -0,0 +1,18 @@
+/*  dom.js
+    Ejercicios Guiados de DOM
+    Autor:Juan Manuel Toscano Reyes <jtoscanoreyes@fundacionloyola.net>
+    licencia: GPL v3 o superior.
+    Año 2021
+*/
+'use strict'
+
+//DOM-Document Object Model
+function ejecutar(){
+  //console.log('Funciona');
+/*  let iNombre=document.getElementById('iNombre')
+  console.log(iNombre.value);*/
+
+  let campos=document.getElementsByTagName('input')
+  console.log(campos);
+
+}
