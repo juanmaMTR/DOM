dfa8e35e8242b7e9ee5d468ce0b760e613c4cb65
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-10-19 12:31:33 +0200

    Commit 2

--- js/dom.js
+++ js/dom.js
@@ -15,4 +15,40 @@ function ejecutar(){
   let campos=document.getElementsByTagName('input')
   console.log(campos);
 
+  let campo=document.getElementsByTagName('input')[1]
+  console.log(campo.value);
+
+  let p=document.querySelector('.c1')
+  console.log(p.innerHTML);
+  console.log(p.outerHTML);
+
+  let spans=document.querySelectorAll('.c1 span')
+  console.log(spans);
+
+  //document.getElementsByClassName
+
+  //Modificación del DOM
+  spans[1].innerHTML='¡Viva la Pepa!'
+
+  //Modificar atributos de estilo
+  spans[0].style.color='blue'
+  spans[0].style.fontSize='20pt'
+
+  //Parrafo que se desplaze de izqu a derecha
+  setInterval(desplazar,100)
+
+
+
+
+}
+let posicion=0
+function desplazar(){
+  let desplazar=document.getElementById('desplazar')
+  desplazar.style.left=`${posicion}px`
+  posicion +=10
+  /*if (desplazar.style.left<1000) {
+    posicion +=10
+  }else{
+    posicion -=10
+  }*/
 }
